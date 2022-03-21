40a0ff9ebb3bacaed6922cacc23cbcd1105092c0
ShugarMummy <100581750+ShugarMummy@users.noreply.github.com>
2022-03-21 16:53:27 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,12 @@
+"Привет Java!"
+var myThing;
+myThing=5;
+myThing="Это строка";
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "ПРивет, ";
+var myName = "Nik!";
+greeting + myName;
+myName.length;
