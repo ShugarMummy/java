481c6052e30f51a481c659bf0b4d35b72e2851d9
ShugarMummy <100581750+ShugarMummy@users.noreply.github.com>
2022-04-30 11:37:47 +0300

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="Ru-Ru">
+
+<head>
+    <meta charset="utf-8">
+    <title>Шаблон страницы</title>
+    <meta name="description" content="The HTML5 sample">
+    <meta name="author" content="from internet">
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+
+<body>
+    <header>
+        <hr>
+        <h1>Заголовок страницы</h1>
+        <hr>
+    </header>
+    <nav>
+        <h2>Навигация</h2>
+        <hr>
+    </nav>
+    <main>
+        <header>
+            <h2>Заголовк контента</h2>
+            <hr>
+        </header>
+
+        <article>
+            <header>
+                <h3>Статья 1</h3>
+            </header>
+            <section>
+                <h4>Введение</h4>
+            </section>
+            <section>
+                <h4>Основная часть</h4>
+            </section>
+            <section>
+                <h4>Заключение</h4>
+            </section>
+            <footer>
+                <hr>
+                <h4>Источник информации</h4>
+                <address>
+                    Ссылка на источник в сети интернет
+                </address>
+            </footer>
+        </article>
+
+    </main>
+    <footer>
+        <hr>
+        <h4>Информация о разработчиках, контакты</h4>
+        <hr>
+    </footer>
+    <script src="js\scripts.js"></script>
+    <body>
+        <script>
+      console.log("Привет, JavaScript");
+      now = new Date();
+      console.log(now); 
+      alert("Сегодня " + now);
+
+       </script>
+    </body>
+
+</body>
+
+</html>
